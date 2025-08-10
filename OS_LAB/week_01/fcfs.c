002b684d1b2d547f300a9c52664dc4601a3f48c6
Bala Satish Malladi <balasatishmalladi@gmail.com>
2025-08-10 23:48:44 +0530

    refator the code

--- OS_LAB/week_01/fcfs.c
+++ OS_LAB/week_01/fcfs.c
@@ -11,42 +11,41 @@ Academic Year: 2025 - 2026
 
 #include <stdio.h>
 
-int main()
-{
-    char process_names[10][10];
-    int burst_time[10];
-    int waiting_time[10];
-    int total_waiting_time = 0;
-    float avg_waiting_time = 0;
-    int i = 0, n = 0;
+int main() {
 
+    int N = 0;
     printf("Enter number of processes: ");
-    scanf("%d", &n);
+    scanf("%d", &N);
 
-    for (i = 0; i < n; i++)
-    {
+    char process_name[N][10];
+    int waiting_time[N];
+    int burst_time[N];
+    int total_waiting_time = 0;
+    float avg_waiting_time = 0.0;
+
+    for (int i = 0; i < N; i++) {
         printf("Enter name of process - %d: ", i + 1);
-        scanf("%s", process_names[i]);
-        printf("Enter process time: ");
+        scanf("%s", process_name[i]);
+        printf("Enter burst time of process - %d: ", i + 1);
         scanf("%d", &burst_time[i]);
     }
 
     waiting_time[0] = 0;
-    for (i = 1; i < n; i++)
-    {
+    for (int i = 1; i < N; i++) {
         waiting_time[i] = waiting_time[i - 1] + burst_time[i - 1];
         total_waiting_time += waiting_time[i];
     }
 
-    avg_waiting_time = (float)total_waiting_time / n;
+    avg_waiting_time = (float)total_waiting_time / N;
 
-    printf("P_name\tP_time\tW_time\n");
-    for (i = 0; i < n; i++)
-    {
-        printf("%s\t%d\t%d\n", process_names[i], burst_time[i], waiting_time[i]);
+    printf("P_Name\t|P_Time\t|W_Time\n");
+    printf("-----------------------\n");
+    for (int i = 0; i < N; i++) {
+        printf("%s\t|%d\t|%d\n", process_name[i], burst_time[i], waiting_time[i]);
     }
 
     printf("Total waiting time: %d\n", total_waiting_time);
-    printf("Average waiting time: %d\n", avg_waiting_time);
+    printf("Average waiting time: %f\n", avg_waiting_time);
+
     return 0;
 }
