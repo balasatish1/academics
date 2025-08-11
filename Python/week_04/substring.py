4ba017c3bc3a4932a9fd0a0ef39c7ecb75ab95fb
Bala Satish Malladi <325103311L03@gvpce.ac.in>
2025-08-11 14:35:15 +0530

    complete substring.py file

--- Python/week_04/substring.py
+++ Python/week_04/substring.py
@@ -1,2 +1,10 @@
 
-print('hello world')
+import re
+
+main_str = input('Enter the main string: ')
+sub_str = input('Enter the substring: ')
+
+if (re.search(sub_str, main_str)):
+  print('"{}" is present in "{}"'.format(sub_str, main_str))
+else:
+  print('"{}" is not present in "{}"'.format(sub_str, main_str))
