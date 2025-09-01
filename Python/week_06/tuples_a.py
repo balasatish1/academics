cc9726f2e5f6f6aa59e0347a2b2dbb5e2c13b0fd
Bala Satish Malladi <325103311L03@gvpce.ac.in>
2025-09-01 14:39:40 +0530

    add tuples programs

---
+++ Python/week_06/tuples_a.py
@@ -0,0 +1,10 @@
+
+tuple_one = ('bala', 19, 'vizag', 'GVP')
+tuple_two = ('satish', 20, 'rajahmundry', 'gmr polytechnic')
+
+print(f'tuple 1: {tuple_one}')
+print(f'tuple 2: {tuple_two}')
+
+concatenated_tuple = tuple_one + tuple_two
+
+print('concatenated tuple: {}'.format(concatenated_tuple))
